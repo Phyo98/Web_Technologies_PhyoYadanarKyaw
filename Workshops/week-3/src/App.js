adbbef664e7119294ad4f37272b188f8a29bfd9c
Phyo98 <135969714+Phyo98@users.noreply.github.com>
2025-03-27 23:32:29 +0630

    add bio and todo test

--- Workshops/week-3/src/App.js
+++ Workshops/week-3/src/App.js
@@ -1,11 +1,17 @@
 import Gallery from './Gallery.js';
 import  Profile  from './Profile.js';
+import TodoList from './TodoList.js';
+import Bio from './Bio.js';
+import Todo from './Todo.js';
 
 export default function App() {
   return (
     <div>
       <Gallery />
       <Profile />
+      <Todo />
+      <Bio />
+      <TodoList />
     </div>
   );
 }
